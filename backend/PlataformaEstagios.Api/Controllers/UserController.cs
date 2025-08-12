95dbdbb5647c0c091a9a38438714319d04da5a71
Lucas <lvksounds@gmail.com>
2025-08-12 13:49:42 -0300

    feat: criando controller pra usuario ja com um endpoint de criação de usuario

---
+++ backend/PlataformaEstagios.Api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlataformaEstagios.Communication.Requests;
+
+namespace PlataformaEstagios.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("/usuario")]
+        public async Task<IActionResult> CreateUser([FromBody] ResquestCreateUserJson request, [FromServices] ICreateUserUseCase useCase)
+        {
+            await useCase.ExecuteAsync(request);
+            return Ok();
+        }
+    }
+}
