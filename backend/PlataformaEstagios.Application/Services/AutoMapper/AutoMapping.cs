fca6cc763fea3c1a2aacbf39712afb813c057456
Lucas <lvksounds@gmail.com>
2025-08-12 13:51:04 -0300

    feat: criando classe de profile pro automapper

---
+++ backend/PlataformaEstagios.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PlataformaEstagios.Communication.Requests;
+
+namespace PlataformaEstagios.Application.Services.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            RequestToDomain();
+            //DomainToResponse();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<ResquestCreateUserJson, Domain.Entities.User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserIdentifier, opt => opt.Ignore())
+                .ForMember(dest => dest.Password, opt => opt.Ignore())
+                .ForMember(dest => dest.UserType, opt => opt.Ignore())
+                .ForMember(dest => dest.Active, opt => opt.Ignore())
+                .ForMember(dest => dest.Nickname, opt => opt.MapFrom(src => src.Nickname))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+                
+        }
+    }
+}
