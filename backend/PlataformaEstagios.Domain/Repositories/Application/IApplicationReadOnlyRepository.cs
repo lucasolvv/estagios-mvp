5bf7571c570d05c219b004b10d3d8761af320e9f
Lucas <lvksounds@gmail.com>
2025-10-02 16:10:43 -0300

    .

--- backend/PlataformaEstagios.Domain/Repositories/Application/IApplicationReadOnlyRepository.cs
+++ backend/PlataformaEstagios.Domain/Repositories/Application/IApplicationReadOnlyRepository.cs
@@ -7,7 +7,7 @@ namespace PlataformaEstagios.Domain.Repositories.Application
         Task<bool> ExistsAsync(Guid vacancyId, Guid candidateId);
         Task<IReadOnlyList<Domain.Entities.Application>> GetRecentApplicationsByCandidateIdAsync(Guid candidateId);
         Task<IReadOnlyList<Domain.Entities.Application>> GetRecentApplicationsByEnterpriseIdAsync(Guid enterpriseId);
-        Task<Entities.Application> GetApplicationByCandidateIdAsync(Guid enterpriseId);
+        Task<Entities.Application?> GetApplicationByApplicationIdAsync(Guid applicationId);
         Task<Entities.Application?> GetByIdAsync(Guid applicationId, CancellationToken ct = default);
     }
 }
